816d05981ae187f9843df61c69d60c9c55b77453
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2023-07-10 02:29:05 +0300

    task_6 complete

--- task_6/task_6.py
+++ task_6/task_6.py
@@ -4,3 +4,31 @@
 # Обязательно учтите год ввода Григорианского календаря
 # В коде должны быть один input и один print
 
+def digit_check():
+    try:
+        lenght = int(input('Введите год:\n'))
+        return lenght
+    except ValueError:
+        print('Это должно быть число!\n')
+        digit_check()
+
+
+def find_year():
+    year = digit_check()
+    if year <= 1918 and year % 4 == 0:
+        print(year, " год высокосный")
+    elif year > 1918 and year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        print(year, " год высокосный")
+    else:
+        print(year, " год не высокосный")
+
+
+find_year()
+
+while True:
+    key = input('Попробуем ещё разок? [y/n]: ')
+    if key == 'y':
+        find_year()
+    else:
+        print('Пока пока!')
+        break
