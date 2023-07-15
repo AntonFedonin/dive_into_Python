2fc1de28a3ee7ebdad6ee3fe496d97434dcb2a09
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2023-07-16 00:29:55 +0300

    Task_1 complete

--- Seminar_3/Task_1.py
+++ Seminar_3/Task_1.py
@@ -1,2 +1,11 @@
 # Дан список повторяющихся элементов. Вернуть список с дублирующимися элементами.
 # В результирующем списке не должно быть дубликатов.
+
+my_list = [9, "Hello", 3, 88, 9, "Hello", 3, 88]
+new_list = []
+
+for i in my_list:
+    if my_list.count(i) > 1 and i not in new_list:
+        new_list.append(i)
+
+print(my_list, '\n', new_list)
