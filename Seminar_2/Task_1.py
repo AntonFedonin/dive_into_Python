c44ee31b24a4bf69c63d5eaac639af467e47f67e
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2023-07-15 13:37:49 +0300

    Task_1 Complete

--- Seminar_2/Task_1.py
+++ Seminar_2/Task_1.py
@@ -2,3 +2,21 @@
 # число и возвращает его шестнадцатеричное
 # строковое представление. Функцию hex
 # используйте для проверки своего результата.
+
+number = int(input("Введите целое число: "))
+
+
+def to_hex(num) -> str:
+    hex_digits = '0123456789ABCDEF'
+    result = ''
+    while num > 0:
+        remainder = num % 16
+        hex_digit = hex_digits[remainder]
+        result = hex_digit + result
+        num //= 16
+    return result
+
+
+print("\n",to_hex(number))
+
+print("\n", hex(number))
