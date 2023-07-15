6a54db91eea5d5d25048a3c9b312f2255f430bf1
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2023-07-15 19:51:25 +0300

    Task_2 complete

--- Seminar_2/Task_2.py
+++ Seminar_2/Task_2.py
@@ -4,4 +4,19 @@
 # и *произведение дробей. Для проверки своего
 # кода используйте модуль fractions.
 
+import fractions
 
+a = input('Введите первую дробь форматом a/b\n')
+b = input('Введите вторую дробь форматом a/b\n')
+
+numerator_1 = int(a.split('/')[0])
+denominator_1 = int(a.split('/')[1])
+numerator_2 = int(b.split('/')[0])
+denominator_2 = int(b.split('/')[1])
+f1 = fractions.Fraction(numerator_1, denominator_1)
+f2 = fractions.Fraction(numerator_2, denominator_2)
+
+sum = f1+f2
+prod=f1*f2
+
+print(f'\n {a} + {b} = {sum}\n {a} * {b} = {prod}')
