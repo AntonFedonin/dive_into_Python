5c43e849de0d37fbd6516c09513da4f2dc94282b
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2023-07-28 19:33:45 +0300

    Task_3 complete

--- Seminar_5/Task_4.py
+++ Seminar_5/Task_4.py
@@ -1,1 +1,12 @@
 # Создайте функцию генератор чисел Фибоначчи (см. Википедию).
+
+def get_fibonacci_nums(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+
+input_num = int(input("Введите число: "))
+result_list = list(get_fibonacci_nums(input_num))
+print(result_list)
