81aa49238f1eb2c465696047f5b69201d4bc84ff
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2023-07-28 19:55:01 +0300

    Task_2 complete

--- Seminar_5/Task_3.py
+++ Seminar_5/Task_3.py
@@ -5,3 +5,15 @@
 # премии в качестве значения. Сумма рассчитывается
 # как ставка умноженная на процент премии
 
+def dict_generator(names, rates, prem):
+    result_dict = {name: rate * float(premia.rstrip("%")) / 100
+                   for name, rate, premia in zip(names, rates, prem)}
+    return result_dict
+
+
+list_names = ["Алексей", "Елена", "Кристина", "Егор"]
+list_rates = [10000, 45000, 38450, 94334]
+list_prem = ["10.25", "17.30", "7.15", "55.10"]
+
+dict = dict_generator(list_names, list_rates, list_prem)
+print(dict)
