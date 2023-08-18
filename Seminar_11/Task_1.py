807d9d52e0479fb51c054124f37282c0a21cbfd0
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2023-08-18 21:53:13 +0300

    Save changes

--- Seminar_11/Task_1.py
+++ Seminar_11/Task_1.py
@@ -12,22 +12,27 @@ class Matrix:
     def __init__(self, arr):
         self.arr = arr
 
-    def __str__(self):
+    def __repr__(self):
         return f'{self.arr}'
 
     def __eq__(self, other):
         return self.arr is other.arr
 
-    def sum (self, other):
-        result = [map(sum, zip(*i)) for i in zip(self.arr, other.arr)]
-        return result
+    def __add__(self, other):
+        lst = []
+        for i in range(len(self.arr)):
+            for j in range(len(self.arr[0])):
+                lst.append(self.arr[i, j] + other.arr[i, j])
+        arr = lst
+
+        return Matrix(arr)
 
 
-a = np.array(Matrix([[1, 2, 3], [4, 5, 6]]))
+a = Matrix([[1, 2, 3], [4, 5, 6]])
 # b = np.array(Matrix([[9, 8, 7], [6, 5, 4]]))
-b = np.array(Matrix([[1, 2, 3], [4, 5, 6]]))
+b = Matrix([[1, 2, 3], [4, 5, 6]])
 
-
+c = a + b
 print(a)
 print(b)
-
+print(c)
