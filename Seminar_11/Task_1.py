707818b60960ce595b69d13363131bee1e0e738e
«Anton.Fedonin» <antoha.fedonin@yandex.ru>
2023-08-18 21:20:21 +0300

    Save changes

--- Seminar_11/Task_1.py
+++ Seminar_11/Task_1.py
@@ -4,18 +4,30 @@
 # ○ сложения,
 # ○ *умножения матриц
 
+import numpy as np
+
 
 class Matrix:
 
-    def __init__(self, arr1, arr2=None):
-        self.arr1 = arr1
-        if arr2 is None:
-            self.arr2 = arr1
-        else:
-            self.arr2 = arr2
+    def __init__(self, arr):
+        self.arr = arr
 
     def __str__(self):
-        return f'{self.arr1}, {self.arr2}'
+        return f'{self.arr}'
+
+    def __eq__(self, other):
+        return self.arr is other.arr
+
+    def sum (self, other):
+        result = [map(sum, zip(*i)) for i in zip(self.arr, other.arr)]
+        return result
 
 
+a = np.array(Matrix([[1, 2, 3], [4, 5, 6]]))
+# b = np.array(Matrix([[9, 8, 7], [6, 5, 4]]))
+b = np.array(Matrix([[1, 2, 3], [4, 5, 6]]))
+
+
+print(a)
+print(b)
 
